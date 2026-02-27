180c396077f7f54fc13c4c8db8c3a3c1826e2ea4
Jacky-ChunChi-Yang <77chunchi@gmail.com>
2026-02-26 20:46:18 -0800

    adding middleware

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import indexRouter from "./routes/index.js";
+import logging from "./middleware/logging.js";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -9,9 +10,12 @@ const PORT = Number(process.env.PORT) || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use("/", logging);
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use("/", indexRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${String(PORT)}`);
+  console.log(
+    `Server running on http://localhost:${String(PORT)} at ${new Date().toLocaleTimeString()}`,
+  );
 });
