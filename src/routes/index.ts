f03393e981f1245df2e25a4655630f3afb079405
Shuo-WenTsaiDoris <doris200404@gmail.com>
2026-03-03 19:48:55 -0800

    code organize

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
+import db from "../db/connection.js";
+
 const router = Router();
 
 const __filename = fileURLToPath(import.meta.url);
@@ -17,6 +19,28 @@ router.get("/about", (_req, res) => {
 
 router.get("/rules", (_req, res) => {
   res.sendFile(path.join(__dirname, "..", "..", "public", "page", "rules.html"));
+});
+
+// POST: Write a message to the database
+router.post("/db-test", async (req, res) => {
+  try {
+    const testMessage = "Gin Rummy Connection Success!";
+    await db.none("INSERT INTO milestone5_test(message) VALUES($1)", [testMessage]);
+    res.json({ message: "Saved to gin_db!" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// GET Route: Reads from the database
+router.get("/db-test", async (req, res) => {
+  try {
+    const data = await db.any("SELECT * FROM milestone5_test ORDER BY id DESC");
+    res.json({ success: true, data });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
 });
 
 router.get("/:id", (req, res) => {
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,6 @@ import { fileURLToPath } from "url";
 import indexRouter from "./routes/index.js";
 import logging from "./middleware/logging.js";
 import dotenv from "dotenv";
-import db from "./db/connection.js";
 
 dotenv.config();
 
@@ -16,28 +15,6 @@ const __dirname = path.dirname(__filename);
 
 app.use("/", logging);
 app.use(express.static(path.join(__dirname, "..", "public")));
-
-// POST: Write a message to the database
-app.post("/db-test", async (req, res) => {
-  try {
-    const testMessage = "Gin Rummy Connection Success!";
-    await db.none("INSERT INTO milestone5_test(message) VALUES($1)", [testMessage]);
-    res.json({ message: "Saved to gin_db!" });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-// GET Route: Reads from the database
-app.get("/db-test", async (req, res) => {
-  try {
-    const data = await db.any("SELECT * FROM milestone5_test ORDER BY id DESC");
-    res.json({ success: true, data });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: err.message });
-  }
-});
 
 app.use("/", indexRouter);
 
